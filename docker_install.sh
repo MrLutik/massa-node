6ac1aaa5465488f27c2c28016b697d878c578e0b
MrLutik <yaievgeniy@gmail.com>
2021-10-12 22:44:07 +0300

    docker installation script

---
+++ docker_install.sh
@@ -0,0 +1,12 @@
+apt update
+#Install packages to use repos
+apt install apt-transport-https ca-certificates curl gnupg lsb-release
+
+#Add official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io
+
